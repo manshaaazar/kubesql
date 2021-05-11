077e89aff2ef1803b897440d0c4d53c4b4750857
manshaaazar <manshaehsan000@gmail.com>
2021-05-11 17:05:16 +0500

    cli

--- src/helpers/delete.js
+++ src/helpers/delete.js
@@ -6,18 +6,74 @@ module.exports = {
     resource = resource.split("=");
     resource = _.capitalize(resource[1]);
     resourceName = resourceName.split("=");
-    const object = {
-      apiVersion: "",
-      kind: "",
-      metadata: { name: "" },
-    };
-    if (resource === "Namespace") {
-      object.apiVersion = "v1";
-      object.kind = resource;
-      object.metadata.name = resourceName[1];
+    if (resource === "Role" || resource === "RoleBinding") {
+      const object = {
+        apiVersion: "rbac.authorization.k8s.io/v1",
+        kind: resource,
+        metadata: { name: resourceName[1], namespace: namespace },
+      };
+      deleteKubernetesResourceDefault(object)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+    } else if (resource === "Cluster" || resource === "ClusterRoleBinding") {
+      const object = {
+        apiVersion: "rbac.authorization.k8s.io/v1",
+        kind: resource,
+        metadata: { name: resourceName[1] },
+      };
+      deleteKubernetesResourceDefault(object)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+    } else if (resource === "Deployment") {
+      const object = {
+        apiVersion: "apps/v1",
+        kind: resource,
+        metadata: { name: resourceName[1], namespace: namespace },
+      };
+      deleteKubernetesResourceDefault(object)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+    } else if (resource === "Namespace") {
+      const object = {
+        apiVersion: "v1",
+        kind: resource,
+        metadata: { name: resourceName[1] },
+      };
+      deleteKubernetesResourceDefault(object)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+    } else if (resource === "iConfig") {
+      const gitResource = {
+        apiVersion: "tekton.dev/v1alpha1",
+        kind: "PipelineResource",
+        metadata: {
+          name: `${resourceName}-git`,
+          namespace: namespace,
+        },
+      };
+      const ImageResource = {
+        apiVersion: "tekton.dev/v1alpha1",
+        kind: "PipelineResource",
+        metadata: {
+          name: `${resourceName}-image`,
+          namespace: namespace,
+        },
+      };
+      deleteKubernetesResourceDefault(gitResource)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+      deleteKubernetesResourceDefault(ImageResource)
+        .then((res) => tableGenerator.deleteSuccessTable(res.body))
+        .catch((err) => tableGenerator.deleteErrTable(err.body));
+    } else {
+      const object = {
+        apiVersion: "v1",
+        kind: resource,
+        metadata: { name: resourceName[1], namespace: namespace },
+      };
+      deleteKubernetesResourceDefault(object)
+        .then((res) => console.log(tableGenerator.deleteSuccessTable(res.body)))
+        .catch((err) => console.log(tableGenerator.deleteErrTable(err.body)));
     }
-    deleteKubernetesResourceDefault(object)
-      .then((res) => console.log(tableGenerator.deleteSuccessTable(res.body)))
-      .catch((err) => console.log(tableGenerator.deleteErrTable(err.body)));
   },
 };
